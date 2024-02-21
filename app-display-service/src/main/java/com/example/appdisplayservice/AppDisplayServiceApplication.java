fd4c55e4e7818118e50748b157646413e6df2904
Kexin (Clare) Chen <108605930+kexincchen@users.noreply.github.com>
2024-02-21 23:01:34 +1100

    added modules

---
+++ app-display-service/src/main/java/com/example/appdisplayservice/AppDisplayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.appdisplayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDisplayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppDisplayServiceApplication.class, args);
+    }
+
+}
---
+++ app-display-service/src/test/java/com/example/appdisplayservice/AppDisplayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appdisplayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDisplayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
